c65a3c347770700ffbfedf9e3a1de4a60eb41e9a
Marat N <nurieff@gmail.com>
2019-01-07 01:44:11 +0500

    v1.1.0

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let Plugin = require('gulp-query').Plugin
   , node_path = require('path')
   , webpack = require("webpack")
+  , TerserPlugin = require('terser-webpack-plugin')
 ;
 
 class WebpackPlugin extends Plugin {
 
   static method() {
-    return 'webpack';
+    return 'webpackBuble';
   }
 
   webpackConfig() {
@@ -18,8 +19,81 @@ class WebpackPlugin extends Plugin {
         filename: null
       },
       module: {
-        rules: []
+        rules: [
+          {parser: {requireEnsure: false}},
+        ]
       }
+    };
+  }
+
+  webpackOptimize()
+  {
+    return {
+      minimize: true,
+      minimizer: [
+        // This is only used in production mode
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              // we want terser to parse ecma 8 code. However, we don't want it
+              // to apply any minfication steps that turns valid ecma 5 code
+              // into invalid ecma 5 code. This is why the 'compress' and 'output'
+              // sections only apply transformations that are ecma 5 safe
+              // https://github.com/facebook/create-react-app/pull/4234
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              // Disabled because of an issue with Uglify breaking seemingly valid code:
+              // https://github.com/facebook/create-react-app/issues/2376
+              // Pending further investigation:
+              // https://github.com/mishoo/UglifyJS2/issues/2011
+              comparisons: false,
+              // Disabled because of an issue with Terser breaking valid code:
+              // https://github.com/facebook/create-react-app/issues/5250
+              // Pending futher investigation:
+              // https://github.com/terser-js/terser/issues/120
+              inline: 2,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              // Turned on because emoji and regex is not minified properly using default
+              // https://github.com/facebook/create-react-app/issues/2488
+              ascii_only: true,
+            },
+          },
+          // Use multi-process parallel running to improve the build speed
+          // Default number of concurrent runs: os.cpus().length - 1
+          parallel: true,
+          // Enable file caching
+          cache: true,
+          sourceMap: false,
+          // chunkFilter: (chunk) => {
+          //   // Exclude uglification for the `vendor` chunk
+          //   if (chunk.name === 'vendor') {
+          //     return false;
+          //   }
+          //
+          //   return true;
+          // },
+        }),
+      ],
+      // Automatically split vendor and commons
+      // https://twitter.com/wSokra/status/969633336732905474
+      // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      splitChunks: {
+        //chunks: 'all',
+        chunks: 'async',
+        name: false,
+      },
+      // Keep the runtime chunk seperated to enable long term caching
+      // https://twitter.com/wSokra/status/969679223278505985
+      //runtimeChunk: true
     };
   }
 
@@ -60,35 +134,68 @@ class WebpackPlugin extends Plugin {
         myDevConfigMin.devtool = false;
       }
 
+      myDevConfigMin.mode = this.isProduction() && !full ? 'production' : 'development';
+
+      if (!this.isProduction()) {
+        myDevConfigMin.output.pathinfo = true;
+        //myDevConfigMin.output.publicPath = '/';
+      }
+
       myDevConfigMin.module.rules.push({
         test: /\.jsx?$/,
-        exclude: [/node_modules/, /public/, new RegExp(path_to)],
+        exclude: [/node_modules/, new RegExp(path_to)],
         use: {
           loader: require.resolve('buble-loader')
         }
       });
 
       myDevConfigMin.module.rules.push({
-        test: /\.css$/,
-        exclude: [/node_modules/, /public/, new RegExp(path_to)],
-        loaders: ['style-loader', 'css-loader'].map(require.resolve)
-      });
-
-      myDevConfigMin.module.rules.push({
-        test: /\.s[ac]ss$/,
-        exclude: [/node_modules/, /public/, new RegExp(path_to)],
-        loaders: ['style-loader', 'css-loader', 'sass-loader'].map(require.resolve)
+        test: /\.s?css$/,
+        exclude: [/node_modules/, new RegExp(path_to)],
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              sourceMap: sourceMap,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                require('postcss-preset-env')({
+                  autoprefixer: {
+                    flexbox: 'no-2009',
+                    //grid: true.
+                    browsers: [
+                      '> 1%',
+                      'last 3 versions'
+                    ]
+                  },
+                  stage: 3,
+                })
+              ],
+              sourceMap: sourceMap
+            }
+          },
+          {
+            loader: require.resolve('sass-loader'),
+            options: {
+              sourceMap: sourceMap,
+              //includePaths: [path_from]
+            }
+          }
+        ]
       });
 
       if (!full && this.isProduction()) {
+        myDevConfigMin.optimization = this.webpackOptimize();
         myDevConfigMin.plugins = [
-          new webpack.LoaderOptionsPlugin({
-            minimize: true,
-            debug: false
-          }),
-          new webpack.optimize.UglifyJsPlugin({
-            sourceMap: sourceMap
-          }),
+
         ];
       }
 
